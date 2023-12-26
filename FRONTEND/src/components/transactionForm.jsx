200261d5f53631ae7ee5540a8f266aff9a1c9ff9
clyde <clydesolas1@gmail.com>
2023-12-27 00:25:29 +0800

    Backedn and frontend: Insert transaction,..cors adjustment in backend code for consistency

--- FRONTEND/src/components/transactionForm.jsx
+++ FRONTEND/src/components/transactionForm.jsx
@@ -1,20 +1,40 @@
 import React, { useState, useEffect } from 'react';
-import { Form, Button, Container, Row, Col } from 'react-bootstrap';
+import { 
+  Form, 
+  Button, 
+  Container, 
+  Row, 
+  Col, 
+  Modal 
+} from 'react-bootstrap';
 import '../assets/css/global.css';
+import axios from 'axios';
+import session from '../components/session.jsx';
+import * as Icon from 'react-bootstrap-icons';
+
 const TransactionForm = () => {
   const [transactionType, setTransactionType] = useState('');
   const [allocationType, setAllocationType] = useState('');
-  const [particular, setParticular] = useState('');
-  const [orNumber, setOrNumber] = useState('');
+  let [particular, setParticular] = useState('');
+  let [orNumber, setOrNumber] = useState('');
   const [amount, setAmount] = useState('');
   const [quantity, setQuantity] = useState('');
   const [total, setTotal] = useState('');
   const [remark, setRemark] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [showModal, setShowModal] = useState(false);
 
   const toggleFields = () => {
     setParticular('');
     setOrNumber('');
   };
+
+  const handleModalClose = () => {
+    setShowModal(false);
+    clearForm();
+  };
+
+  const handleModalShow = () => setShowModal(true);
 
   const updateTotal = () => {
     const amountValue = parseFloat(amount) || 0;
@@ -51,90 +71,198 @@ const TransactionForm = () => {
     setRemark('');
   };
 
-  const submitForm = (e) => {
-    e.preventDefault();
-    // Add your form submission logic here
-    alert('Form submitted!');
+  const isAuthenticated = session();
+  console.log(isAuthenticated);
+
+  async function submitForm(event) {
+    event.preventDefault();
+    axios.defaults.withCredentials = true;
+
+    try {
+      if (transactionType === 'INFLOW') {
+        particular = allocationType+' RECEIVED'; 
+        orNumber = 'N/A';
+      }
+      const response = await axios.post('http://localhost:8001/transaction/save', {
+        transactionType: transactionType,
+        allocationType: allocationType,
+        amount: amount,
+        quantity: quantity,
+        total: total,
+        remark: remark,
+        particular: particular,
+        orNumber: orNumber
+      });
+
+      if (response.data === 'Success') {
+        console.log('Success');
+        handleModalShow();
+      } else {
+        console.error('Unexpected response:', response.data);
+        setErrorMessage('Unexpected response from the server');
+      }
+    } catch (error) {
+      if (error.response) {
+        console.error('Server responded with an error status:', error.response.status);
+        setErrorMessage('Error: ' + (error.response.data.message || 'Unknown error'));
+      } 
+      if (error.request) {
+        console.error('No response received from the server');
+        setErrorMessage('No response received from the server');
+      } else {
+        console.error('Error setting up the request:', error.message);
+        setErrorMessage('Error setting up the request');
+      }
+    }
   };
 
   return (
     <Container className="form-container mt-1 p-4 rounded-4 container-bg">
-      <h4 className='d-flex justify-content-center mb-3'>Transaction Form</h4>
-      <Form onSubmit={submitForm}>
-        <Row className="mb-3">
-          <Col sm={6}>
-            <Form.Label>Transaction Type:</Form.Label>
-            <Form.Select value={transactionType} onChange={(e) => { setTransactionType(e.target.value); toggleFields(); }}>
-              <option value="" disabled>Select..</option>
-              <option value="Inflow">Inflow</option>
-              <option value="Outflow">Outflow</option>
-            </Form.Select>
-          </Col>
-          <Col sm={6}>
-            <Form.Label>Allocation Type:</Form.Label>
-            <Form.Select value={allocationType} onChange={(e) => setAllocationType(e.target.value)}>
-              <option value="" disabled>Select..</option>
-              <option value="Collection">Collection</option>
-              <option value="IGP">IGP</option>
-              <option value="Donation">Donation</option>
-            </Form.Select>
-          </Col>
-        </Row>
+      <h4 className="d-flex justify-content-center mb-3">Transaction Form</h4>
+      <Form>
+        <form method="POST">
+          <Row className="mb-3">
+            <Col sm={6}>
+              <Form.Label>Transaction Type:</Form.Label>
+              <Form.Select
+                value={transactionType}
+                id="transactionType"
+                name="transactionType"
+                onChange={(e) => {
+                  setTransactionType(e.target.value);
+                  toggleFields();
+                }}
+              >
+                <option value="" disabled>Select..</option>
+                <option value="INFLOW">Inflow</option>
+                <option value="OUTFLOW">Outflow</option>
+              </Form.Select>
+            </Col>
+            <Col sm={6}>
+              <Form.Label>Allocation Type:</Form.Label>
+              <Form.Select
+                value={allocationType}
+                onChange={(e) => setAllocationType(e.target.value)}
+              >
+                <option value="" disabled>Select..</option>
+                <option value="COLLECTION">Collection</option>
+                <option value="IGP">IGP</option>
+                <option value="DONATION">Donation</option>
+              </Form.Select>
+            </Col>
+          </Row>
 
-        {transactionType === 'Outflow' && (
-          <>
-            <Row className="mb-3">
-              <Col sm={6}>
-                <Form.Label>Particular:</Form.Label>
-                <Form.Select value={particular} onChange={(e) => setParticular(e.target.value)} required>
-                  <option value="" disabled>Select..</option>
-                  <option value="Food Expense">Food Expense</option>
-                  <option value="Supplies Expense">Supplies Expense</option>
-                  {/* Add other options for Outflow */}
-                </Form.Select>
-              </Col>
-              <Col sm={6}>
-                <Form.Label>OR No.:</Form.Label>
-                <Form.Control type="text" id="orNumber" value={orNumber} onChange={(e) => setOrNumber(e.target.value)} required />
-              </Col>
-            </Row>
-          </>
-        )}
+          {transactionType === 'OUTFLOW' && (
+            <>
+              <Row className="mb-3">
+                <Col sm={6}>
+                  <Form.Label>Particular:</Form.Label>
+                  <Form.Select
+                    value={particular}
+                    onChange={(e) => setParticular(e.target.value)}
+                    required
+                  >
+                     <option value="" disabled>Select..</option>
+                      <option value="FOOD EXPENSE">Food Expense</option>
+                      <option value="SUPPLIES EXPENSE">Supplies Expense</option>
+                      <option value="TRANSPORTATION EXPENSE">Transportation Expense</option>
+                      <option value="RENT EXPENSE">Rent Expense</option>
+                      <option value="REPRESENTATION EXPENSE">Representation Expense</option>
+                      <option value="MEDICAL EXPENSE">Medical Expense</option>
+                      <option value="COMMUNICATION EXPENSE">Communication Expense</option>
+                      <option value="MISCELLANEOUS EXPENSE">Miscellaneous Expense</option>
+                      <option value="TOKEN EXPENSE">Token Expense</option>
+                  </Form.Select>
+                </Col>
+                <Col sm={6}>
+                  <Form.Label>OR No.:</Form.Label>
+                  <Form.Control
+                    type="text"
+                    id="orNumber"
+                    value={orNumber}
+                    onChange={(e) => setOrNumber(e.target.value)}
+                    required
+                  />
+                </Col>
+              </Row>
+            </>
+          )}
 
-        <Row className="mb-3">
-          <Col sm={6}>
-            <Form.Label>Amount:</Form.Label>
-            <Form.Control type="text" id="amount" value={amount} pattern="^\d+(\.\d{1,2})?$" required onChange={handleAmountChange} />
-          </Col>
-          <Col sm={6}>
-            <Form.Label>Quantity:</Form.Label>
-            <Form.Control type="text" id="quantity" value={quantity} pattern="^\d+$" onChange={handleQuantityChange} />
-          </Col>
-        </Row>
+          <Row className="mb-3">
+            <Col sm={6}>
+              <Form.Label>Amount:</Form.Label>
+              <Form.Control
+                type="text"
+                id="amount"
+                value={amount}
+                pattern="^\d+(\.\d{1,2})?$"
+                required
+                onChange={handleAmountChange}
+              />
+            </Col>
+            <Col sm={6}>
+              <Form.Label>Quantity:</Form.Label>
+              <Form.Control
+                type="text"
+                id="quantity"
+                value={quantity}
+                pattern="^\d+$"
+                onChange={handleQuantityChange}
+              />
+            </Col>
+          </Row>
 
-        <Row className="mb-3"w>
-          <Col sm={6}>
-            <Form.Label>Total:</Form.Label>
-            <Form.Control type="text" id="total" value={total} readOnly />
-          </Col>
-        </Row>
+          <Row className="mb-3">
+            <Col sm={6}>
+              <Form.Label>Total:</Form.Label>
+              <Form.Control type="text" id="total" value={total} readOnly />
+            </Col>
+          </Row>
 
-        <Row  className="mb-3">
-          <Col sm={12}>
-            <Form.Label>Remark:</Form.Label>
-            <Form.Control as="textarea" id="remark" value={remark} onChange={(e) => setRemark(e.target.value)} required />
-          </Col>
-        </Row>
+          <Row className="mb-3">
+            <Col sm={12}>
+              <Form.Label>Remark:</Form.Label>
+              <Form.Control
+                as="textarea"
+                id="remark"
+                value={remark}
+                onChange={(e) => setRemark(e.target.value)}
+                required
+              />
+            </Col>
+          </Row>
 
-        <Row className="mt-2">
-          <Col sm={12} className='d-flex justify-content-end'>
-            <div>
-              <Button className="mx-2" variant="secondary" onClick={clearForm}>Clear</Button>
-              <Button variant="success" className='button-bg' type="submit">Submit</Button>
-            </div>
-          </Col>
-        </Row>
+          <Row className="mt-2">
+            <Col sm={12} className="d-flex justify-content-end">
+              <div>
+                <Button className="mx-2" variant="secondary" onClick={clearForm}>
+                  Clear
+                </Button>
+                <Button variant="success" className="button-bg" onClick={submitForm}>
+                  Submit
+                </Button>
+              </div>
+            </Col>
+          </Row>
+        </form>
       </Form>
+      <Modal show={showModal} onHide={handleModalClose} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>
+            <h3 className="d-flex justify-content-center" style={{ color: 'green' }}>
+              Success <Icon.Check2Circle />
+            </h3>
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p className="d-flex justify-content-center">Transaction has been submitted successfully.</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="success" onClick={handleModalClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </Container>
   );
 };
--- FRONTEND/src/components/transactionTable.jsx
+++ FRONTEND/src/components/transactionTable.jsx
@@ -2,24 +2,27 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import DataTable from 'react-data-table-component';
 import { Form, Button, Container, Modal } from 'react-bootstrap';
-import * as Icon from "react-bootstrap-icons";
+import * as Icon from 'react-bootstrap-icons';
+
 const TransactionTable = () => {
   const [data, setData] = useState([]);
   const [searchText, setSearchText] = useState('');
   const [selectedRow, setSelectedRow] = useState(null);
   const [showModal, setShowModal] = useState(false);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('http://localhost:8001/transaction/fetchAll');
-        setData(response.data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://localhost:8001/transaction/fetchAll');
+      setData(response.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
 
+  useEffect(() => {
     fetchData();
+    const intervalId = setInterval(fetchData, 1000);
+    return () => clearInterval(intervalId);
   }, []);
 
   const formatTransactionDate = (row) => {
@@ -35,13 +38,35 @@ const TransactionTable = () => {
   };
 
   const columns = [
-    { name: 'Transaction ID', selector: 'transactionId', sortable: true, minWidth: '90px', grow: 5 },
+    { name: 'Transaction ID', selector: 'transactionId', sortable: true, minWidth: '137px', grow: 5 },
     { name: 'Date', selector: 'transactionDate', sortable: true, minWidth: '165px', cell: (row) => formatTransactionDate(row) },
-    { name: 'Type', selector: 'transactionType', sortable: true },
+    {
+      name: 'Type',
+      selector: 'transactionType',
+      sortable: true,
+      minWidth: '115px',
+      cell: (row) => (
+        <span>
+          {row.transactionType === 'INFLOW' ? (
+            <>
+              <div className='text-success'>
+                Inflow <Icon.ArrowUp />
+              </div>
+            </>
+          ) : (
+            <>
+              <div className='text-danger'>
+                Outflow <Icon.ArrowDown />
+              </div>
+            </>
+          )}
+        </span>
+      ),
+    },
     { name: 'Amount', selector: 'amount', sortable: true },
     { name: 'Quantity', selector: 'quantity', sortable: true },
     { name: 'Total', selector: 'total', sortable: true },
-    { name: 'Particular', selector: 'particular', sortable: true, minWidth: '150px', grow: 5 },
+    { name: 'Particular', selector: 'particular', sortable: true, minWidth: '160px', grow: 5 },
     { name: 'OR No.', selector: 'orNumber', sortable: true },
     { name: 'Remark', selector: 'remark', sortable: true, minWidth: '150px', grow: 5 },
     {
@@ -51,20 +76,15 @@ const TransactionTable = () => {
       minWidth: '50px',
       grow: 5,
       cell: (row) => (
-       <Button variant="link" size="sm" onClick={() => handleOpenModal(row)}>
-          <Icon.Pencil /> {/* Bootstrap pencil icon */}
+        <Button variant='link' size='sm' onClick={() => handleOpenModal(row)}>
+          <Icon.Pencil color='green' />
         </Button>
       ),
     },
   ];
 
   const filteredData = data.filter((row) =>
-    columns.some(
-      (column) =>
-        String(row[column.selector])
-          .toLowerCase()
-          .includes(searchText.toLowerCase())
-    )
+    columns.some((column) => String(row[column.selector]).toLowerCase().includes(searchText.toLowerCase()))
   );
 
   const handleOpenModal = (row) => {
@@ -77,15 +97,10 @@ const TransactionTable = () => {
   };
 
   return (
-    <Container className="form-container mt-1 p-3 py-4 rounded-4 container-bg">
-      <h4 className="d-flex justify-content-center">Breakdown of Cash Inflows and Outflows</h4>
-      <Form.Group controlId="search" className="mb-2">
-        <Form.Control
-          type="text"
-          placeholder="Search..."
-          value={searchText}
-          onChange={(e) => setSearchText(e.target.value)}
-        />
+    <Container className='form-container mt-1 p-3 pt-4 rounded-4 container-bg'>
+      <h4 className='d-flex justify-content-center'>Breakdown of Cash Inflows and Outflows</h4>
+      <Form.Group controlId='search' className='my-3'>
+        <Form.Control type='text' placeholder='Search...' value={searchText} onChange={(e) => setSearchText(e.target.value)} />
       </Form.Group>
       <DataTable
         columns={columns}
@@ -102,19 +117,19 @@ const TransactionTable = () => {
         <Modal.Body>
           {selectedRow && (
             <div>
-                <p>Transaction ID: {selectedRow.transactionId}</p>
-                <p>Date: {formatTransactionDate(selectedRow)}</p>
-                <p>Type: {selectedRow.transactionType}</p>
-                <p>Particular: {selectedRow.particular}</p>
-                <p>Amount: {selectedRow.amount}</p>
-                <p>Quantity: {selectedRow.quantity}</p>
-                <p>Total: {selectedRow.total}</p>
-                <p>Remark: {selectedRow.remark}</p>
+              <p>Transaction ID: {selectedRow.transactionId}</p>
+              <p>Date: {formatTransactionDate(selectedRow)}</p>
+              <p>Type: {selectedRow.transactionType}</p>
+              <p>Particular: {selectedRow.particular}</p>
+              <p>Amount: {selectedRow.amount}</p>
+              <p>Quantity: {selectedRow.quantity}</p>
+              <p>Total: {selectedRow.total}</p>
+              <p>Remark: {selectedRow.remark}</p>
             </div>
           )}
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleCloseModal}>
+          <Button variant='secondary' onClick={handleCloseModal}>
             Close
           </Button>
         </Modal.Footer>
