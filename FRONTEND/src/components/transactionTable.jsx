fd63cb7dee1d4d85da9c211941759b3842ac13b1
clyde <clydesolas1@gmail.com>
2023-12-26 19:29:36 +0800

    Added view modal action in transaction table

--- FRONTEND/src/components/transactionTable.jsx
+++ FRONTEND/src/components/transactionTable.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import DataTable from 'react-data-table-component';
-import { Form, Button, Container } from 'react-bootstrap';
-
+import { Form, Button, Container, Modal } from 'react-bootstrap';
+import * as Icon from "react-bootstrap-icons";
 const TransactionTable = () => {
   const [data, setData] = useState([]);
   const [searchText, setSearchText] = useState('');
+  const [selectedRow, setSelectedRow] = useState(null);
+  const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -19,7 +21,7 @@ const TransactionTable = () => {
 
     fetchData();
   }, []);
-  
+
   const formatTransactionDate = (row) => {
     const transactionDate = new Date(row.transactionDate);
     return transactionDate.toLocaleString('en-US', {
@@ -33,15 +35,27 @@ const TransactionTable = () => {
   };
 
   const columns = [
-    { name: 'Transaction ID', selector: 'transactionId', sortable: true , minWidth: '90px' , grow: 5},
-    { name: 'Date', selector: 'transactionDate', sortable: true, minWidth: '165px', cell: (row) => formatTransactionDate(row), },
+    { name: 'Transaction ID', selector: 'transactionId', sortable: true, minWidth: '90px', grow: 5 },
+    { name: 'Date', selector: 'transactionDate', sortable: true, minWidth: '165px', cell: (row) => formatTransactionDate(row) },
     { name: 'Type', selector: 'transactionType', sortable: true },
     { name: 'Amount', selector: 'amount', sortable: true },
     { name: 'Quantity', selector: 'quantity', sortable: true },
     { name: 'Total', selector: 'total', sortable: true },
-    { name: 'Particular', selector: 'particular', sortable: true , minWidth: '150px' , grow: 5},
+    { name: 'Particular', selector: 'particular', sortable: true, minWidth: '150px', grow: 5 },
     { name: 'OR No.', selector: 'orNumber', sortable: true },
-    { name: 'Remark', selector: 'remark', sortable: true , minWidth: '150px' , grow: 5},
+    { name: 'Remark', selector: 'remark', sortable: true, minWidth: '150px', grow: 5 },
+    {
+      name: 'Action',
+      selector: 'actionModal',
+      sortable: false,
+      minWidth: '50px',
+      grow: 5,
+      cell: (row) => (
+       <Button variant="link" size="sm" onClick={() => handleOpenModal(row)}>
+          <Icon.Pencil /> {/* Bootstrap pencil icon */}
+        </Button>
+      ),
+    },
   ];
 
   const filteredData = data.filter((row) =>
@@ -53,10 +67,19 @@ const TransactionTable = () => {
     )
   );
 
+  const handleOpenModal = (row) => {
+    setSelectedRow(row);
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
   return (
     <Container className="form-container mt-1 p-3 py-4 rounded-4 container-bg">
       <h4 className="d-flex justify-content-center">Breakdown of Cash Inflows and Outflows</h4>
-      <Form.Group controlId="search" className='mb-2'>
+      <Form.Group controlId="search" className="mb-2">
         <Form.Control
           type="text"
           placeholder="Search..."
@@ -70,8 +93,32 @@ const TransactionTable = () => {
         pagination
         paginationPerPage={5}
         highlightOnHover
-        paginationRowsPerPageOptions={[5,10, 20, 30, 40, 50]}
+        paginationRowsPerPageOptions={[5, 10, 20, 30, 40, 50]}
       />
+      <Modal show={showModal} onHide={handleCloseModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>View</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {selectedRow && (
+            <div>
+                <p>Transaction ID: {selectedRow.transactionId}</p>
+                <p>Date: {formatTransactionDate(selectedRow)}</p>
+                <p>Type: {selectedRow.transactionType}</p>
+                <p>Particular: {selectedRow.particular}</p>
+                <p>Amount: {selectedRow.amount}</p>
+                <p>Quantity: {selectedRow.quantity}</p>
+                <p>Total: {selectedRow.total}</p>
+                <p>Remark: {selectedRow.remark}</p>
+            </div>
+          )}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseModal}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </Container>
   );
 };
